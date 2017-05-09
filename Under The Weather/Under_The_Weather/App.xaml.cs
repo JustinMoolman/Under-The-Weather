13ea0a4f2ab2203c1bbf8b4960e527a11ee80f89
JustinMoolman <justinmoolman@gmail.com>
2017-05-09 12:29:57 +0200

    Under The Weather
    Original

---
+++ Under The Weather/Under_The_Weather/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Under_The_Weather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Under_The_Weather.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Under The Weather/Under_The_Weather/View1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Under_The_Weather
+{
+    public partial class View1 : ContentView
+    {
+        public View1()
+        {
+            InitializeComponent();
+        }
+    }
+}
